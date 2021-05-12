58e39e76b870364c2b167930a8ef9715666888ca
aliibrahim095 <4liebrahim@gmail.com>
2021-05-12 22:38:53 +0200

    updating profile pic

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -4,6 +4,9 @@ import { UserContext } from "../../App";
 const Profile = () => {
   const [mypics, setPics] = useState([]);
   const { state, dispatch } = useContext(UserContext);
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
   useEffect(() => {
     fetch("/mypost", {
       headers: {
@@ -15,6 +18,33 @@ const Profile = () => {
         setPics(result.mypost);
       });
   }, []);
+
+  useEffect(() => {
+    if (image) {
+      const data = new FormData();
+      data.append("file", image);
+      data.append("upload_preset", "meanblog");
+      data.append("cloud_name", "meanblogger");
+      fetch("https://api.cloudinary.com/v1_1/meanblogger/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUrl(data.url);
+          console.log(data)
+          localStorage.setItem("user", JSON.stringify({...state,pic:data.url}))
+          dispatch({type:"UPDATEPIC",payload:data.url})
+          window.location.reload();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [image]);
+  const updatePic = (file) => {
+    setImage(file);
+  };
   return (
     <>
       {mypics ? (
@@ -36,6 +66,18 @@ const Profile = () => {
                 }}
                 src={state ? state.pic : "loading..."}
               />
+              <div className="file-field input-field">
+                <div className="btn #64b5f6 blue darken-1">
+                  <span>update pic</span>
+                  <input
+                    type="file"
+                    onChange={(e) => updatePic(e.target.files[0])}
+                  />
+                </div>
+                <div className="file-path-wrapper">
+                  <input className="file-path validate" type="text" />
+                </div>
+              </div>
             </div>
             <div>
               <h4>{state ? state.name : "Loading"}</h4>
@@ -51,8 +93,8 @@ const Profile = () => {
                   {mypics.length}{" "}
                   {mypics.length == 1 ? <b>Post</b> : <b>Posts</b>}
                 </h6>
-                <h6> {state ? state.followers.length : "0"} followers</h6>
-                <h6> {state ? state.following.length : "0"} following</h6>
+                <h6> {state? state.followers.length : "0"} followers</h6>
+                <h6> {state? state.following.length : "0"} following</h6>
               </div>
             </div>
           </div>
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -21,4 +21,4 @@ export const reducer = (state,action)=>{
         }
     }
     return state
-} 
+}
